bf3b5d5b0b3c166c08495ec015e4ec19836c2453
Sander Van Aken <sander.van-aken@flixbus.com>
2024-06-23 09:07:35 +0200

    feat: session-state and auto-app rerun example
    Streamlit reloads the page each time the user interacts with the input elements, and does this from
    top to bottom. Hence, this means that we can also re-use some of the input provided. To do so, we
    can collect the value from the `session_state`. Note that the `session_state` will be empty at the
    start of a new browser session.

--- streamlitapp/hello_world.py
+++ streamlitapp/hello_world.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 st.title("Conference Programme Explorer")
 
@@ -18,10 +19,23 @@ version of the programme, check the [conference website](https://euro2024cph.dk/
 
 st.write("## Basic input elements")
 
+# Streamlit reloads on every user interaction, and does this from top-to-bottom in the script. This means
+# that the random number generated here will be different on every reload.
+st.write(f"Random number: {random.randint(0, 100)}")
+
+# Streamlit reloads the page each time the user interacts with the input elements. Hence, this means
+# that we can also re-use some of the input provided. For example, we can use the provided text input
+# at an earlier stage in the application by collecting it from the `session_state`. Note that the `session_state`
+# will be empty at the start of a new browser session.
+text_input_value = st.session_state.get("text_input_value", None)
+st.write(f"Text input value: {text_input_value}")
+
 # We can capture the input provided by the user in a variable and as such use it later on.
 number_input_value = st.number_input("Enter a positive number", value=1, min_value=0, step=1)
 
-text_input_value = st.text_input("Enter some text")
+# To capture the provided user input and re-use it at other points in the app, we can set the `key` of
+# the input element and store the value in the `session_state`.
+text_input_return = st.text_input("Enter some text", value=None, key="text_input_value")
 
 st.write(f"Number input value: {number_input_value}")
-st.write(f"Text input value: {text_input_value}")
+st.write(f"Text input value: {text_input_return}")
