0b7588f8737f1f4741c767e3163b2dfcf73d45f1
Sander Van Aken <sander.van-aken@flixbus.com>
2024-06-23 08:57:04 +0200

    feat: number and text input field examples
    Streamlit has a range of different components to enable user input and
    users interacting with the app. The provided values can be captured and
    used later on

--- streamlitapp/hello_world.py
+++ streamlitapp/hello_world.py
@@ -14,3 +14,14 @@ In the following commits, or using the different pages of the app, you will see 
 to build a simple application to explore the EURO 2024 conference programme. For an up-to-date
 version of the programme, check the [conference website](https://euro2024cph.dk/)"""
 )
+
+
+st.write("## Basic input elements")
+
+# We can capture the input provided by the user in a variable and as such use it later on.
+number_input_value = st.number_input("Enter a positive number", value=1, min_value=0, step=1)
+
+text_input_value = st.text_input("Enter some text")
+
+st.write(f"Number input value: {number_input_value}")
+st.write(f"Text input value: {text_input_value}")
