801505c844693efd69ba6c48bdb71a64b1c17999
Sander Van Aken <sander.van-aken@flixbus.com>
2024-06-23 08:47:18 +0200

    feat: initial hello world kind of example
    To run the app locally, navigate in your terminal to the project directory and simply run
    `streamlit run streamlitapp/hello_world.py`. This will open the Streamlit app in your
    browser

---
+++ streamlitapp/hello_world.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Conference Programme Explorer")
+
+st.write("Hello World")
+
+st.markdown(
+    """Welcome to the **Conference Programme Explorer!**
+
+Streamlit is a great tool for building interactive data applications, and amongst
+others allows the use of markdown to format text.
+
+In the following commits, or using the different pages of the app, you will see how
+to build a simple application to explore the EURO 2024 conference programme. For an up-to-date
+version of the programme, check the [conference website](https://euro2024cph.dk/)"""
+)
