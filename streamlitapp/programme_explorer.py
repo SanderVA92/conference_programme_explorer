bbc2c95cb866ec2f212fe7010a7590f245761027
Sander Van Aken <sander.van-aken@flixbus.com>
2024-06-27 08:25:56 +0200

    refactor: move filters to the sidebar

--- streamlitapp/programme_explorer.py
+++ streamlitapp/programme_explorer.py
@@ -12,27 +12,27 @@ st.set_page_config(layout="wide")
 
 
 def display_multiselect_filters(df_programme: pd.DataFrame) -> None:
-    col_multiselect_filters = st.columns(3)
+    # col_multiselect_filters = st.columns(3)
 
     # Add a timeslot filter to the page itself
     potential_timeslots = data_utils.get_unique_timeslots(df_programme)
-    col_multiselect_filters[0].multiselect("Timeslot(s)", potential_timeslots, key="selected_timeslots")
+    st.multiselect("Timeslot(s)", potential_timeslots, key="selected_timeslots")
 
     # Add a stream filter to the page itself, in the column next to the timeslot filter
     potential_streams = data_utils.get_unique_streams(df_programme, filter_by_state=True)
     preset_streams = data_utils.get_preselected_streams(potential_streams)
-    col_multiselect_filters[1].multiselect("Stream(s)", potential_streams, key="selected_streams", default=preset_streams)
+    st.multiselect("Stream(s)", potential_streams, key="selected_streams", default=preset_streams)
 
     # Add a keywords filter to the page itself
     potential_keywords = data_utils.get_unique_keywords(df_programme)
-    col_multiselect_filters[2].multiselect("Keyword(s)", potential_keywords, key="selected_keywords")
+    st.multiselect("Keyword(s)", potential_keywords, key="selected_keywords")
 
 
 def display_text_based_filters() -> None:
     col_text_filters = st.columns(2)
 
-    col_text_filters[0].text_input("Search in title ...", key="title_search")
-    col_text_filters[1].text_input("Search in abstract ...", key="abstract_search")
+    st.text_input("Search in title ...", key="title_search")
+    st.text_input("Search in abstract ...", key="abstract_search")
 
 
 def display_optimization_model_filters(df_programme: pd.DataFrame) -> None:
@@ -82,8 +82,6 @@ def conference_browsing_tab(df_complete_programme: pd.DataFrame, **kwargs) -> No
     container = kwargs.get('container', st)
 
     with container:
-        display_multiselect_filters(df_complete_programme)
-        display_text_based_filters()
 
         # Before the programme can be displayed, we need to filter it based on the user's selection, using the session state
         df_filtered = data_filter.filter_programme_based_on_state(df_complete_programme)
@@ -163,6 +161,11 @@ def main() -> None:
         all_tabs_to_show = ['Browse Conference Programme', 'Optimize Your Schedule']
 
     main_page_tabs = st.tabs(all_tabs_to_show)
+
+    with st.sidebar:
+        display_multiselect_filters(df_complete_programme)
+
+        display_text_based_filters()
 
     conference_browsing_tab(df_complete_programme, container=main_page_tabs[0])
 
